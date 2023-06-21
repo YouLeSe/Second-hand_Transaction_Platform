41c5035cd502122f13ac8f515e575a943fd1ecb9
Sliver_0812 <116070589+PwwwLe@users.noreply.github.com>
2023-06-21 09:59:52 +0800

    初始化uni_app和导航栏
    初始化uni_app和导航栏

---
+++ uni_app-Second-hand_Transaction_Platform/unpackage/dist/dev/mp-weixin/pages/home/home.js
@@ -0,0 +1,12 @@
+"use strict";
+const common_vendor = require("../../common/vendor.js");
+const _sfc_main = {
+  data() {
+    return {};
+  }
+};
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return {};
+}
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "C:/Users/Pwl/Documents/GitHub/Second-hand_Transaction_Platform/uni_app-Second-hand_Transaction_Platform/pages/home/home.vue"]]);
+wx.createPage(MiniProgramPage);
---
+++ uni_app-Second-hand_Transaction_Platform/unpackage/dist/dev/mp-weixin/static/tab_icons/tab_icons.js
@@ -0,0 +1,13 @@
+"use strict";
+const common_vendor = require("../../common/vendor.js");
+const _sfc_main = {
+  data() {
+    return {};
+  },
+  methods: {}
+};
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return {};
+}
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "C:/Users/Pwl/Documents/GitHub/Second-hand_Transaction_Platform/uni_app-Second-hand_Transaction_Platform/static/tab_icons/tab_icons.vue"]]);
+wx.createPage(MiniProgramPage);
